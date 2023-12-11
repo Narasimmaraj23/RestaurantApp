30ca34209eab9e267451f616b10085c705985db2
NARASIMMARAJ <113287661+Narasimmaraj23@users.noreply.github.com>
2023-12-11 19:56:57 +0530

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,7 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAxXtlKXx-BU6OgoH3s_OTyJXogGvyBjOA",
-    authDomain: "restaurantapp-efb04.firebaseapp.com",
-    databaseURL: "https://restaurantapp-efb04-default-rtdb.firebaseio.com",
-    projectId: "restaurantapp-efb04",
-    storageBucket: "restaurantapp-efb04.appspot.com",
-    messagingSenderId: "1095171925794",
-    appId: "1:1095171925794:web:a7acb529f776d0f824677b"
+    
   };
 
 const app = getApps.length >0 ? getApp() : initializeApp(firebaseConfig);
